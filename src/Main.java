b01ad26e1993c9f75b3a3e608d2ab2a7db6a5506
mdfeist <mdfeist@ualberta.ca>
2013-03-19 13:42:25 -0600

    Able to measure time

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
+import java.util.Date;
+
 public class Main {
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		XMLParser xml = new XMLParser("Data10.xml");
+		long lStartTime = new Date().getTime();
+		
+		XMLParser xml = new XMLParser("Data1k.xml");
 		CreateDataFiles.createTerms(xml.getAds());
 		CreateDataFiles.createDates(xml.getAds());
 		CreateDataFiles.createPrices(xml.getAds());
 		CreateDataFiles.createAds(xml.getAds());
+		
+		long lEndTime = new Date().getTime();
+		
+		System.out.println("Time: " + (lEndTime - lStartTime));
 	}
 
 }
