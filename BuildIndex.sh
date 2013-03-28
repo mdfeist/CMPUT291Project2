c6003fae96e0970a60ea51d0cefc3de6f32df1ff
mdfeist <mdfeist@ualberta.ca>
2013-03-28 09:26:27 -0600

    Updated BuildIndex.sh script

--- BuildIndex.sh
+++ BuildIndex.sh
@@ -1,9 +1,9 @@
-sort -k2 -t'-' data/terms.txt -o data/terms.txt;
-sort -t: data/pdates.txt -o data/pdates.txt;
-sort -n -t: data/prices.txt -o data/prices.txt;
-sort -n -t: data/ads.txt -o data/ads.txt;
+sort -u -t: data/terms.txt -o data/terms.txt;
+sort -u -t: data/pdates.txt -o data/pdates.txt;
+sort -u -n -t: data/prices.txt -o data/prices.txt;
+sort -u -n -t: data/ads.txt -o data/ads.txt;
 
-awk -F: '{print $1; print $2}' < data/ads.txt | db_load -T -t hash data/ad.idx;
-awk -F: '{print $1; print $2}' < data/pdates.txt | db_load -T -c duplicates=1 -t btree data/da.idx;
-awk -F: '{print $1; print $2}' < data/prices.txt | db_load -T -c duplicates=1 -t btree data/pr.idx;
-awk -F: '{print $1; print $2}' < data/terms.txt | db_load -T -c duplicates=1 -t btree data/te.idx;
+awk -F: '{print $1; print $2}' < data/ads.txt | sed 's/\\//g' | db_load -T -t hash data/ad.idx;
+awk -F: '{print $1; print $2}' < data/pdates.txt | sed 's/\\//g' | db_load -T -c duplicates=1 -t btree data/da.idx;
+awk -F: '{print $1; print $2}' < data/prices.txt | sed 's/\\//g' | db_load -T -c duplicates=1 -t btree data/pr.idx;
+awk -F: '{print $1; print $2}' < data/terms.txt | sed 's/\\//g' | db_load -T -c duplicates=1 -t btree data/te.idx;
